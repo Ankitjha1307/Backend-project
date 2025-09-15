f6df77aa0c0342e0d616a8c4974a123f0e3ef394
Ankit Jha <mrankitjha007@gmail.com>
2025-09-16 00:06:13 +0530

    multer configured

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,12 +2,12 @@ import multer from "multer";
 
 const storage= multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "/tmp/my-uploads")
+        cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.filename + '-' + uniqueSuffix)
+
+        cb(null, file.originalname) // as the file would be for a very small amount of time on the server before upload so no issues of duplicate names
     }
 })
 
-const upload = multer({storage: storage})
+const upload = multer({storage})
