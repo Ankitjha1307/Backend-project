c4615381631bcd1ca830c521c091d0d530725490
Ankit Jha <mrankitjha007@gmail.com>
2025-10-01 01:03:55 +0530

    adding routes and authentication middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -94,7 +94,7 @@ const registerUser= asyncHandler (async (req,res) => {
 const loginUser= asyncHandler (async (req,res) => {
     const {username, email, password} = req.body
 
-    if(!username || !email){
+    if (!(username || email)){
         throw new ApiError(400, "username or email required")
     }
 
@@ -133,4 +133,28 @@ const loginUser= asyncHandler (async (req,res) => {
     )
 })
 
-export {registerUser, loginUser}
+const logoutUser= asyncHandler (async (req,res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+     const options = {
+        httpOnly: true,
+        secure: true
+     } 
+
+     return res 
+     .status(200)
+     .clearCookie("accessToken", options)
+     .clearCookie("refreshToken", options)
+     .json(new ApiResponse(200, {}, "User logged out successfully"))
+})
+
+export {registerUser, loginUser, logoutUser}
